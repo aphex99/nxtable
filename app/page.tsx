d6e1fa7ebb500d0bd5d2f82eaf2ff5247fd061a9
belovroman90 <belov.roman.90@mail.ru>
2025-07-20 13:56:42 +0700

    Added image for post

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import Image from 'next/image';
+
 export default function Home() {
     return (
         <div className={"h-screen flex justify-center items-center"}>
             <article className="flex flex-col w-sm">
+                <Image
+                    alt={"Julia Fox above the toilet bowl"}
+                    src={'/posts/fox.jpg'}
+                    width={800}
+                    height={500}
+                    priority={true}
+                />
                 <h2 className={"text-2xl font-bold mb-2"}>My first blog post</h2>
                 <p>I don't know what to say. Just will text some words in English. I hope I haven't mistakes in this
-                    simple senses.</p>
+                    simple sentenses.</p>
             </article>
         </div>
     );
