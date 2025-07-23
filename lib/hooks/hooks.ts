e35efa0c316b37a46d3cb1f6bad81765b5b0ee05
belovroman90 <belov.roman.90@mail.ru>
2025-07-23 21:49:48 +0700

    Added RTK. First setup

---
+++ lib/hooks/hooks.ts
@@ -0,0 +1,7 @@
+import {AppDispatch, AppStore, RootState} from "@/lib/store";
+import {useDispatch, useSelector, useStore} from "react-redux";
+
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppStore = useStore.withTypes<AppStore>();
+export const useAppSelector = useSelector.withTypes<RootState>();
