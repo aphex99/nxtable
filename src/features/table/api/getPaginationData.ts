07c6df893d1396885d8b73e80c7a44c9d3a0a9d6
belovroman90 <belov.roman.90@mail.ru>
2025-10-27 17:39:36 +0700

    NX-3 fix: fix pagination fetch

--- src/features/table/api/getPaginationData.ts
+++ src/features/table/api/getPaginationData.ts
@@ -1,22 +1,22 @@
 'use server';
 
-import { createClient } from '@/src/shared/supabase/server';
+import {createClient} from '@/src/shared/supabase/server';
 
 export async function getPaginationData(page: number, countPerPage: number) {
-  const from = page > 1 ? (page - 1) * countPerPage : page;
+  const from = (page - 1) * countPerPage;
   const to = from + countPerPage - 1;
 
   const baseClient = await createClient();
 
-  const { data, count, error } = await baseClient
+  const {data, count, error} = await baseClient
     .from('singers')
-    .select('id, name, email, type', { count: 'exact' })
-    .select()
+    .select('id, name, email, type', {count: 'exact'})
     .range(from, to);
   if (error) {
-    console.error('Error fetching singers: ', error);
-    return { data: [], totalCount: 0 };
+    console.error('Error fetching singers: ', error.message);
+    console.error('Full error: ', error);
+    return {data: [], totalCount: 0};
   }
 
-  return { data, totalCount: count };
+  return {data, totalCount: count};
 }
